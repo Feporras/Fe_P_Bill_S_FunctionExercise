4600a1f7f1c69867a53390f114e18dfba5cb26f2
Feporras <67969032+Feporras@users.noreply.github.com>
2020-07-20 20:53:44 -0500

    Add files via upload

---
+++ monday july 20.playground/Contents.swift
@@ -0,0 +1,10 @@
+func addTwoNumbers(num1: Int, num2: Int) -> Int {
+    var total = 0
+    total = num1 + num2
+    
+    return total
+    
+}
+
+var totalNumber = addTwoNumbers(num1: 90, num2: 456)
+print("90 + 456 = \(addTwoNumbers(num1: 90, num2: 456))")
